0ed7b67c9734300e6bc43e98630146b40ed8bb4b
DucThan25 <nguyenducthan2507@gmail.com>
2023-04-08 02:51:58 +0700

    update footer

--- src/components/FooTer.vue
+++ src/components/FooTer.vue
@@ -4,7 +4,7 @@
         <v-row>
           <v-col cols="12" sm="3">
             <v-card-text class="white--text pt-0">
-              <h3>Giới thiệu</h3>
+              <h3>Giới thiệu 1111</h3>
             </v-card-text>
             <v-card-text class="grey--text pt-0">
               Kínhasaass mắt thời trang Chasmish là thương hiệu uy tín,<br />
